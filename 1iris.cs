3ace3e595fa8bc986bafb5f292b33509c6005e7e
Анна Ященко < >
2016-06-21 19:18:10 +0300

    basu dannuh

---
+++ 1iris.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace iris
+{
+    class Program
+    {
+        public static void dz(double a)
+        {
+            if (a >= 0.1 && a <= 0.6) Console.WriteLine("Iris - setosa"); 
+            if (a > 0.6 && a < 1.7) Console.WriteLine("Iris-versicolor"); 
+            if (a >= 1.8 && a <= 2.5) Console.WriteLine("Iris-virginica");
+            else Console.WriteLine("");
+        }
+
+        /////////////////
+
+        public static void r1(double[][] num, int a)
+        { 
+            //сортировка
+            double[] cup;
+
+            for (int i1 = 0; i1 < num.GetLength(0) - 1; i1++)
+                for (int i = 0; i < num.GetLength(0) - 1; i++)
+                    if (num[i][a] > num[i + 1][a])
+                    {
+                        cup = num[i];
+                        num[i] = num[i + 1];
+                        num[i + 1] = cup;
+                    }
+
+
+
+            for (int i = 0; i < num.GetLength(0); i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    Console.Write(num[i][j] + " ");
+                Console.WriteLine();
+            }
+
+            //обьединение по числам
+            double[,] opr = new double[150,6];
+            int k = 0;
+            
+            for (int i = 0; i < num.GetLength(0) - 1; i++)
+            {
+                if (num[i][a] == num[i + 1][a])
+                {
+                    opr[k, 0] = num[i][a];
+                    opr[k, Convert.ToInt32(num[i][4])]++;
+                }
+                else
+                {
+                    opr[k, 0] = num[i][a];
+                    opr[k, Convert.ToInt32(num[i][4])]++;
+                    k++;
+                }
+            }
+
+            if (num[num.GetLength(0) - 1][a] != num[num.GetLength(0) - 2][a])
+            {
+                opr[k, 0] = num[num.GetLength(0) - 1][a];
+                opr[k, Convert.ToInt32(num[num.GetLength(0) - 1][4])]++;
+                k++;
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < opr.GetLength(1); j++)
+                    Console.Write(opr[i, j] + " ");
+                Console.WriteLine();
+            }
+            
+
+
+            //наилучшее и похибка
+
+
+            for (int i = 0; i < k; i++)
+            {
+                    if (opr[i, 1] >= opr[i, 2] && opr[i, 1] >= opr[i, 3])
+                    {
+                        opr[i, 4] = 1;
+                        opr[i, 5] = opr[i, 2] + opr[i, 3];
+                        goto qq;
+                    }
+
+                    if (opr[i, 2] >= opr[i, 1] && opr[i, 2] >= opr[i, 3])
+                    {
+                        opr[i, 4] = 2;
+                        opr[i, 5] = opr[i, 1] + opr[i, 3];
+                        goto qq;
+                    }
+
+                    if (opr[i, 3] >= opr[i, 2] && opr[i, 3] >= opr[i, 2])
+                    {
+                        opr[i, 4] = 3;
+                        opr[i, 5] = opr[i, 2] + opr[i, 1];
+                        goto qq;
+                    }
+                qq: { }
+            }
+
+            //for (int i = 0; i < k; i++)
+            //{
+            //    for (int j = 0; j < opr.GetLength(1); j++)
+            //        Console.Write(opr[i, j] + " ");
+            //    Console.WriteLine();
+            //}
+
+
+            //обьединение в группы
+
+            int[,] lich = new int[1,3];
+            int tr = 0;
+            int pohubka = 0;
+
+
+            for (int i = 0; i < k; i++)
+            {
+                if (lich[0, 0] == 3 || lich[0, 1] == 3 || lich[0, 2] == 3)
+                {
+                    if (lich[0, 0] == 3) tr = blabla(i, opr, 0);
+                    if (lich[0, 1] == 3) tr = blabla(i, opr, 1);
+                    if (lich[0, 2] == 3) tr = blabla(i, opr, 2);
+
+                    for (int j = i - 3; j <= tr; j++)
+                        pohubka += Convert.ToInt32(opr[j, 5]);
+
+                        i = tr;
+                    lich[0, 0] = 0; lich[0, 1] = 0; lich[0, 2] = 0;
+                }
+                else
+                {
+                    lich[0, Convert.ToInt32(opr[i, 4]) - 1]++;
+                }
+
+                    
+            }
+
+            Console.WriteLine(pohubka);
+
+
+
+
+            //for (int i = 0; i < num.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < 5; j++)
+            //        Console.Write(num[i][j] + " ");
+            //    Console.WriteLine();
+            //}
+
+        }
+
+        public static int blabla(int ii, double[,] opr, int ir)
+        {
+            Console.Write( opr[ii - 3, 0]+"-");
+
+            while (opr[ii, 4] == opr[ii + 1, 4])
+            {
+                ii++;
+            }
+
+            Console.WriteLine(opr[ii, 0]);
+
+            return ii;
+        }
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            StreamReader fil = new StreamReader(@"D:\3data base\iris.data");
+            {
+                string line = fil.ReadToEnd();
+                string newline = line.Replace("\n", "$");
+
+                string[] dog = newline.Split('$');
+
+                string[][] mass = new string[dog.Length][];
+                for (int i = 0; i < dog.Length; i++)
+                    mass[i] = dog[i].Split(',');
+
+                for (int i = 0; i < dog.Length; i++)
+                    for (int j = 0; j < mass[i].Length; j++)
+                        mass[i][j] = mass[i][j].Replace(".", ",");
+
+
+
+                for (int i = 0; i < mass.GetLength(0)-2; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                        Console.Write(mass[i][j] + " ");
+                    Console.WriteLine();
+                }
+
+
+
+
+                double[][] numbers = new double[dog.Length-2][];
+                double[] t = new double[5];
+
+                //Console.WriteLine(numbers.GetLength(0));
+
+                for (int i = 0; i < numbers.GetLength(0); i++)
+                {
+                    numbers[i] = new double[5];
+
+                    for (int j = 0; j < 5; j++)
+                    {
+                        
+                        if (j < 4)
+                        {
+                            numbers[i][j] = Convert.ToDouble(mass[i][j]);
+                            //Console.WriteLine(mass[i][j]);
+                            //Console.WriteLine(numbers[i][j]);
+                        }
+                        else switch (mass[i][4])
+                            {
+                                case "Iris-setosa":
+                                    numbers[i][4] = 1;
+                                    break;
+                                case "Iris-versicolor":
+                                    numbers[i][4] = 2;
+                                    break;
+                                case "Iris-virginica":
+                                    numbers[i][4] = 3;
+                                    break;
+                            }
+
+
+                    }
+                    //numbers[i] = t;
+                    //for (int r = 0; r < 5; r++)
+                    //    numbers[i][r] = t[r];
+                }
+
+
+                //int setosa, versicolor, virginica;
+                //setosa = 0; versicolor = 0; virginica = 0;
+
+                //for (int i = 0; i < numbers.GetLength(0); i++)
+                //    for (int j = 0; j < numbers.GetLength(1); j++)
+                //        if (numbers[i, 3] >= 1.8)
+                //        {
+                //            switch (mass[i][4])
+                //            {
+                //                case "Iris-setosa":
+                //                    setosa++;
+                //                    break;
+                //                case "Iris-versicolor":
+                //                    versicolor++;
+                //                    break;
+                //                case "Iris-virginica":
+                //                    virginica++;
+                //                    break;
+                //            }
+                //        }
+
+
+                    //Console.WriteLine(setosa+" "+versicolor+" "+virginica);
+
+
+
+                //dz!!!!
+
+                //string lin = Console.ReadLine();
+                //string[] m = lin.Split(',');
+                //dz(Convert.ToDouble(m[m.Length - 1].Replace(".", ",")));
+
+
+                for (int i = 0; i < numbers.GetLength(0); i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                        Console.Write(numbers[i][j] + " ");
+                    Console.WriteLine();
+                }
+
+                r1(numbers, 4);
+
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 2divancon.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace newdivandcon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fil = new StreamReader(@"D:\3data base\dc.txt");
+            {
+                //working with data:
+                string line = fil.ReadToEnd();
+
+                string[] dog = line.Split('*');
+
+                int count = 0;
+                foreach (char a in dog[0])
+                    if (a == ',') count++;
+
+                string[][] mass = new string[dog.Length][];
+                for (int i = 0; i < dog.Length; i++)
+                    mass[i] = dog[i].Split(',');
+                
+                Tree tree = new Tree();
+                Do(mass, tree, count);
+
+                tree.Show();
+            }
+            Console.ReadKey();
+        }
+
+
+
+
+        public static double[] GINI(int st, string[][] mas, int count)
+        {
+            List<string> names = new List<string>();
+            for (int i = 1; i < mas.Length; i++)
+            {
+                if (!names.Contains(mas[i][st]))
+                {
+                    names.Add(mas[i][st]);
+                }
+            }
+
+            double[,] m = new double[names.Count, count];
+
+            foreach (string s in names)
+                for (int j = 1; j < mas.Length; j++)
+                {
+                    if (mas[j][st] == s)
+                    {
+                        switch (mas[j][count])
+                        {
+                            case "yes":
+                                m[names.IndexOf(s), 0]++;
+                                m[names.IndexOf(s), 2]++;
+                                break;
+                            case "no":
+                                m[names.IndexOf(s), 1]++;
+                                m[names.IndexOf(s), 2]++;
+                                break;
+                        }
+                    }
+
+                }
+
+            double all = 0;
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                m[i, 3] = 1 - (Math.Pow(m[i, 0] / m[i, 2], 2) + Math.Pow(m[i, 1] / m[i, 2], 2));
+                all += m[i, 2];
+            }
+
+
+
+            double gini = 0;
+
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                gini += (m[i, 2] / all) * m[i, 3];
+            }
+
+            double[] ginimasiv = new double[m.GetLength(0) + 1];
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                ginimasiv[i] = m[i, count - 1];
+            }
+            ginimasiv[ginimasiv.Length - 1] = gini;
+
+            return ginimasiv;
+
+        }
+
+
+
+
+
+
+        public static void Do(string[][] mas, Tree tree, int count)
+        {
+            //choosing best brunch
+            List<double> ginilist = new List<double>();
+            double[] ginigini = null;
+            for (int i = 0; i < count; i++)
+            {
+                ginigini = GINI(i, mas, count);
+                ginilist.Add(ginigini[ginigini.Length - 1]);
+                //Console.WriteLine(ginilist[i]);
+            }
+
+            int index = ginilist.IndexOf(ginilist.Min());
+            double[] ginimasiv = GINI(index, mas, count);
+
+            //Adding to the tree
+            List<string> children = new List<string>();
+            for (int i = 1; i < mas.Length; i++)
+            {
+                if (!children.Contains(mas[i][index]))
+                    children.Add(mas[i][index]);
+            }
+
+            tree.Add(mas[0][index], children);
+
+            //if one child is perfect add it's result to the tree
+            //else make recursion
+            for (int i = 0; i < ginimasiv.Length - 1; i++)
+            {
+                if (ginimasiv[i] == 0)
+                {
+                    //you may know yes or no value
+                    string yesno = null;
+                    for (int j = 1; j < mas.Length; j++)
+                        if (mas[j][index] == children[i])
+                        {
+                            yesno = mas[j][count];
+                            break;
+                        }
+
+                    tree.Add(yesno, null);
+                    
+                }
+                else
+                {
+                    //need to cut your masiv
+                    int lich = 0;
+                    for (int j = 1; j < mas.Length; j++)
+                        if (mas[j][index] == children[i]) lich++;
+
+                    string[][] newmas = new string[lich+1][];
+
+                    newmas[0] = mas[0];
+                    int ii = 1;
+                    for (int j = 1; j < mas.Length; j++)
+                    {
+                        if (mas[j][index] == children[i])
+                            newmas[ii++] = mas[j];
+                    }
+
+                    //for (int q = 0; q < newmas.Length; q++)
+                    //{
+                    //    for (int w = 0; w < count; w++)
+                    //        Console.WriteLine(newmas[q][w]);
+                    //    Console.WriteLine();
+                    //}
+
+                    Do(newmas, tree, count);
+                }
+            }
+        }
+    }
+}
